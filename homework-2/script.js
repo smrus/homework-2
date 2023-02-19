4ec33a95e386439e8f125beaeec02fa975f0719a
smrus <118106745+smrus@users.noreply.github.com>
2023-02-19 17:03:09 +0300

    Add files via upload

---
+++ homework-2/script.js
@@ -0,0 +1,26 @@
+//homework 2
+
+const money = 1000 //Nuber;
+console.log (typeof money);
+const profit = Фриланс // String;
+console.log (typeof profit);
+const expenses = "питание,проезд" //String;
+console.log ("длина" + expenses.length);
+const purpose = 2000 //Nuber;
+const period = 10 //Nuber;
+console.log ("накопить сумму за ${period} месяцев");
+console.log ("цель заработать ${purpose} доларов");
+let budgetDay = (money / 30);
+console.log (budgetDay);
+
+let money = prompt (money);
+console.log (money);
+let expenses = prompt (expenses);
+console.log (expenses);
+let amount = prompt (amount);
+console.log (amount);
+let deposit = confirm ( deposit);
+console.log ( deposit);
+let budgetMonth = (money - amount);
+console.log (budgetMonth);
+
